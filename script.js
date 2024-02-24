ce2ee909b7aa05b98ea2078996cbdc73fb79746f
iamayush25 <ayush22@navgurukul.org>
2024-02-25 00:42:17 +0530

    Commit

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+  let sidePanel = document.getElementById("mySidepanel");
+  let currentWidth = sidePanel.style.width;
+
+  if (currentWidth === "0px" || currentWidth === "") {
+    sidePanel.style.width = "250px";
+  } else {
+    sidePanel.style.width = "0";
+  }
 }
 
-function closeNav() {
+function closeBtn() {
   document.getElementById("mySidepanel").style.width = "0";
 }
 
-function Resume() {
-  window.location.href = "/";
-}
-
-let linksPage = document.querySelectorAll(".primary-navigation > li>a");
-
-const outputElement = document.getElementById("output");
-const text = ["Greetings, ", "I'm Ayush Sharma."];
-let currentIndex = 0;
-const delay = 3000;
-
-function animateText() {
-  outputElement.innerHTML = null;
-
-  outputElement.innerHTML += text[currentIndex] + " ";
-  currentIndex = (currentIndex + 1) % text.length;
-}
-
-setInterval(animateText, delay);
-
-animateText(0);
-
-function redirectToEmail() {
-  // Replace recipient@example.com with the actual email address
-  var emailRecipient = "ayush22@navgurukul.org";
-
-  // Use the 'mailto' URL scheme to open the default email client
-  window.open("mailto:" + emailRecipient, "_blank");
-}
