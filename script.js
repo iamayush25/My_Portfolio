c4e096eda4e0261436b30477f4958bd13655cafb
iamayush25 <ayush22@navgurukul.org>
2024-03-17 21:09:15 +0530

    Update SidePanel

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function openNav() {
   let currentWidth = sidePanel.style.width;
     // if sidepannel width 0px or empty (Like close) that's why we give 250px
   if (currentWidth === "0px" || currentWidth === "") {
-    sidePanel.style.width = "250px";
+    sidePanel.style.width = "100%";
+    sidePanel.style.height = "5%";
   } 
     // if sidepannel width !0px mean sidepanel is open that's why we give 0px  
     else {
